0b7fad5b493c44c880138e02b9895b54ef88fd05
zhouyang <xuepowuming@163.com>
2024-07-20 15:45:26 +0800

    first commit

---
+++ src/components/Notify/index.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { Bell } from "@element-plus/icons-vue"
+</script>
+
+<template>
+  <div class="notify">
+    <el-dropdown class="right-menu-item">
+      <el-badge :value="50" :max="99" :hidden="true">
+        <el-icon :size="20">
+          <Bell />
+        </el-icon>
+      </el-badge>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>
+            <span style="display: block">全部消息</span>
+          </el-dropdown-item>
+          <el-dropdown-item>
+            <span style="display: block">产品消息</span>
+          </el-dropdown-item>
+          <el-dropdown-item>
+            <span style="display: block">安全消息</span>
+          </el-dropdown-item>
+          <el-dropdown-item>
+            <span style="display: block">服务消息</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.notify {
+  margin-right: 10px;
+}
+
+.notify-history {
+  text-align: center;
+  padding-top: 12px;
+  border-top: 1px solid var(--el-border-color);
+}
+</style>
---
+++ src/layouts/components/Logo/index.vue
@@ -0,0 +1,74 @@
+<script lang="ts" setup>
+import { useLayoutMode } from "@/hooks/useLayoutMode"
+
+const VITE_APP_TITLE = import.meta.env.VITE_APP_TITLE
+
+interface Props {
+  collapse?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  collapse: true
+})
+
+const { isLeft, isTop } = useLayoutMode()
+</script>
+
+<template>
+  <div class="layout-logo-container" :class="{ collapse: props.collapse, 'layout-mode-top': isTop }">
+    <transition name="layout-logo-fade">
+      <router-link class="layout-logo-content">
+        <img
+          src="http://144.48.83.77:9002/xh_admin/images/%E7%B3%BB%E7%BB%9F%E6%A1%86%E6%9E%B6/u38.svg"
+          class="layout-logo-text"
+          alt=""
+        />
+        <div class="layout-text">{{ VITE_APP_TITLE }}</div>
+      </router-link>
+    </transition>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout-logo-container {
+  position: relative;
+  width: 100%;
+  height: var(--v3-header-height);
+  line-height: var(--v3-header-height);
+  text-align: center;
+  overflow: hidden;
+  .layout-logo {
+    display: none;
+  }
+  .layout-logo-text {
+    width: 20%;
+    margin-right: 10px;
+  }
+  .layout-logo-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .layout-text {
+    font-size: 20px;
+    color: #fcfcfc;
+  }
+}
+
+.layout-mode-top {
+  height: var(--v3-navigationbar-height);
+  line-height: var(--v3-navigationbar-height);
+}
+
+.collapse {
+  .layout-logo {
+    width: 32px;
+    height: 32px;
+    vertical-align: middle;
+    display: inline-block;
+  }
+  .layout-logo-text {
+    display: none;
+  }
+}
+</style>
