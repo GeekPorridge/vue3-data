cbdf33550d8d607d44d2ae32d6331b822bc123ed
zhiyao.he <h@192.168.0.101>
2024-07-26 19:21:56 +0800

    glylb-editModal:管理员列表弹窗交互

---
+++ src/views/authority-manage/components/glylb-editModal.vue
@@ -0,0 +1,265 @@
+<template>
+  <el-dialog destroy-on-close v-model="open" :title="title[dialogType]" class="qxgl_glylb_edite-model__dialog">
+    <el-card :shadow="'never'">
+      <el-form ref="formRef" :label-position="'top'" :model="formInline">
+        <div class="top-card">
+          <el-row :gutter="24">
+            <el-col :span="12">
+              <el-form-item prop="yhm" label="用户名">
+                <el-input v-model="formInline.yhm" placeholder="请输入文本字段" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item prop="js" label="角色">
+                <el-select v-model="formInline.js" placeholder="选择选项">
+                  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+
+          <el-row :gutter="24">
+            <el-col :span="12">
+              <el-form-item prop="dlzh" label="登录密码">
+                <el-input v-model="formInline.dlzh" placeholder="请输入文本字段" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item prop="dlmm" label="登录账号">
+                <el-input v-model="formInline.dlmm" placeholder="请输入文字字段" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </div>
+      </el-form>
+    </el-card>
+
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button class="cancel" @click="closeModal">取消</el-button>
+        <el-button class="save" type="primary" @click="saveModel">保存</el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+/**
+ * @record 当前编辑的数据
+ */
+import { ElMessage } from "element-plus"
+import { ref, reactive, watch, defineProps, defineExpose, defineEmits } from "vue"
+
+const props = defineProps<{
+  record: Object // 编辑的列表数据
+}>()
+
+const emit = defineEmits(["updateList"])
+const formRef = ref(null) // 表单ref
+
+const open = ref(false)
+const data = ref(props.record || {})
+const dialogType = ref("edite") // 弹窗类型  编辑：edite  新增：add
+const title = {
+  edite: "编辑",
+  add: "新增"
+}
+
+const options = [
+  {
+    value: "角色1",
+    label: "角色1"
+  },
+  {
+    value: "角色2",
+    label: "角色2"
+  },
+  {
+    value: "角色3",
+    label: "角色3"
+  }
+]
+
+// 表单模拟字段
+const formInline = reactive({
+  yhm: "", // 用户名
+  js: "", // 角色
+  dlzh: "", // 登录账号
+  dlmm: "" // 登录密码
+})
+
+// 清空表单
+const clearFrom = () => {
+  if (formRef.value) {
+    formRef.value.resetFields()
+    formRef.value.clearValidate()
+  }
+}
+// 打开弹框
+const openModal = (type = "edite") => {
+  dialogType.value = type
+  open.value = true
+}
+
+const saveData = () => {
+  console.log(formInline, "保存数据：请求api")
+
+  return {
+    success: true,
+    data: "响应数据"
+  }
+}
+
+// 关闭弹框
+const closeModal = () => {
+  clearFrom()
+  open.value = false
+}
+
+//  保存
+const saveModel = async () => {
+  const { success, data } = await saveData()
+  if (success) {
+    ElMessage({
+      message: `${title[dialogType.value]}成功`,
+      type: "success"
+    })
+    emit("updateList", data)
+    closeModal()
+    return
+  }
+
+  ElMessage({
+    message: "编辑失败",
+    type: "error"
+  })
+}
+
+watch(
+  () => props.record,
+  (newRecord) => {
+    data.value = newRecord || {}
+  },
+  { immediate: true } // 在组件初始化时也调用
+)
+
+defineExpose({ openModal })
+</script>
+
+<!-- <style lang="scss" scoped>
+:deep(.el-dialog) {
+  padding: 0;
+}
+</style> -->
+ß
+<style lang="scss">
+.qxgl_glylb_edite-model__dialog {
+  padding: 0 0 16px;
+  border-radius: 5px;
+  overflow: hidden;
+  .el-dialog__header {
+    height: 50px;
+    background-color: rgba(245, 245, 245, 1);
+    display: flex;
+    padding: 0 16px;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+
+  .el-dialog__body {
+    padding: 0 16px 0;
+  }
+
+  .dialog-footer {
+    padding: 16px;
+    .el-button {
+      width: 80px;
+      height: 30px;
+      font-size: 12px;
+    }
+
+    .cancel {
+      color: #999;
+    }
+  }
+
+  .el-card {
+    border: transparent;
+  }
+
+  .el-card__body {
+    margin-top: 20px;
+    padding: 0;
+    .bottom-card,
+    .top-card {
+      padding: 20px;
+      border: 1px solid rgba(237, 237, 237, 1);
+      border-radius: 10px;
+
+      .tab-title {
+        font-size: 14px;
+        color: #666;
+        line-height: 28px;
+        span {
+          font-size: 11px;
+          color: #999;
+        }
+      }
+    }
+
+    .bottom-card {
+      margin-top: 20px;
+    }
+
+    .el-form-item__label {
+      font-size: 14px;
+      color: #666;
+      line-height: 28px;
+    }
+
+    .el-input__wrapper:hover {
+      box-shadow: 0 0 0 1px #409eff inset;
+    }
+
+    .el-input__inner {
+      height: 40px;
+    }
+
+    input::-webkit-input-placeholder {
+      /* 使用webkit内核的浏览器 */
+      font-size: 14px;
+      color: #ccc;
+    }
+    input:-moz-placeholder {
+      /* Firefox版本4-18 */
+      font-size: 14px;
+      color: #ccc;
+    }
+    input::-moz-placeholder {
+      /* Firefox版本19+ */
+      font-size: 14px;
+      color: #ccc;
+    }
+    input:-ms-input-placeholder {
+      /* IE浏览器 */
+      font-size: 14px;
+      color: #ccc;
+    }
+  }
+
+  .el-tabs {
+    border: transparent;
+    .el-tabs__header {
+      background: none;
+      border: transparent;
+      margin-bottom: 20px;
+      .el-tabs__item {
+        border: none;
+      }
+    }
+    .el-tabs__content {
+      padding: 0;
+    }
+  }
+}
+</style>
