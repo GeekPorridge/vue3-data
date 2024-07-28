58c7fb6d203f44e004cc6993e2f240bd44977261
zhiyao.he <h@192.168.0.103>
2024-07-28 19:06:53 +0800

    gglb-editModal:公告列表弹窗富文本

--- src/views/content-manage/components/gglb-editModal.vue
+++ src/views/content-manage/components/gglb-editModal.vue
@@ -79,12 +79,27 @@
                     <template #label>
                       <div class="tab-title">{{ `内容多语言${formTabInline.active}` }}</div>
                     </template>
-                    <el-input v-model="formTabInline.nr" placeholder="富文本" />
                   </el-form-item>
                 </el-col>
               </el-row>
             </el-tab-pane>
           </el-tabs>
+
+          <div class="bottom-card">
+            <Toolbar
+              style="border-bottom: 1px solid #ccc"
+              :editor="editorRef"
+              :defaultConfig="toolbarConfig"
+              mode="default"
+            />
+            <Editor
+              style="height: 500px; overflow-y: hidden"
+              v-model="valueHtml"
+              :defaultConfig="editorConfig"
+              @onCreated="handleCreated"
+              mode="default"
+            />
+          </div>
         </div>
       </el-form>
     </el-card>
@@ -105,7 +120,19 @@
  * @record 当前编辑的数据
  */
 import { ElMessage } from "element-plus"
-import { ref, reactive, watch, defineProps, defineExpose, defineEmits } from "vue"
+import "@wangeditor/editor/dist/css/style.css"
+import { Editor, Toolbar } from "@wangeditor/editor-for-vue"
+import {
+  ref,
+  reactive,
+  watch,
+  defineProps,
+  defineExpose,
+  defineEmits,
+  onBeforeUnmount,
+  shallowRef,
+  onMounted
+} from "vue"
 
 const props = defineProps<{
   from: string
@@ -187,6 +214,7 @@ const tabChange = (key: number) => {
     formTabRef.value.clearValidate()
   }
   formTabInline.active = ++key
+  valueHtml.value = "<p></p>"
 }
 
 // 清空表单
@@ -239,6 +267,32 @@ const saveModel = async () => {
     message: "编辑失败",
     type: "error"
   })
+}
+
+const editorRef = shallowRef()
+
+// 内容 HTML
+const valueHtml = ref("<p></p>")
+
+// 模拟 ajax 异步获取内容
+onMounted(() => {
+  // setTimeout(() => {
+  //   valueHtml.value = "<p>模拟 Ajax 异步设置内容</p>"
+  // }, 1500)
+})
+
+const toolbarConfig = {}
+const editorConfig = { placeholder: "请输入内容..." }
+
+// 组件销毁时，也及时销毁编辑器
+onBeforeUnmount(() => {
+  const editor = editorRef.value
+  if (editor == null) return
+  editor.destroy()
+})
+
+const handleCreated = (editor: any) => {
+  editorRef.value = editor // 记录 editor 实例，重要！
 }
 
 watch(
