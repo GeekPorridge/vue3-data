d544a9eff00c3c108263268874b0bceb90b1bee9
zhiyao.he <h@192.168.0.103>
2024-07-29 11:52:26 +0800

    login:冗余代码删除;媒体查询交互

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,10 +4,7 @@ import { useRouter } from "vue-router"
 import { useUserStore } from "@/store/modules/user"
 import { type FormInstance, type FormRules } from "element-plus"
 import { User, Lock } from "@element-plus/icons-vue"
-import { getLoginCodeApi } from "@/api/login"
 import { type LoginRequestData } from "@/api/login/types/login"
-// import ThemeSwitch from "@/components/ThemeSwitch/index.vue"
-// import Owl from "./components/Owl.vue"
 import { useFocus } from "./hooks/useFocus"
 
 const router = useRouter()
@@ -23,8 +20,7 @@ const codeUrl = ref("")
 /** 登录表单数据 */
 const loginFormData: LoginRequestData = reactive({
   username: "admin",
-  password: "12345678",
-  code: ""
+  password: "12345678"
 })
 /** 登录表单校验规则 */
 const loginFormRules: FormRules = {
@@ -32,8 +28,7 @@ const loginFormRules: FormRules = {
   password: [
     { required: true, message: "请输入密码", trigger: "blur" },
     { min: 8, max: 16, message: "长度在 8 到 16 个字符", trigger: "blur" }
-  ],
-  code: [{ required: true, message: "请输入验证码", trigger: "blur" }]
+  ]
 }
 /** 登录逻辑 */
 const handleLogin = () => {
@@ -46,7 +41,6 @@ const handleLogin = () => {
           router.push({ path: "/" })
         })
         .catch(() => {
-          createCode()
           loginFormData.password = ""
         })
         .finally(() => {
@@ -57,25 +51,10 @@ const handleLogin = () => {
     }
   })
 }
-/** 创建验证码 */
-const createCode = () => {
-  // 先清空验证码的输入
-  loginFormData.code = ""
-  // 获取验证码
-  codeUrl.value = ""
-  getLoginCodeApi().then((res) => {
-    codeUrl.value = res.data
-  })
-}
-
-/** 初始化验证码 */
-createCode()
 </script>
 
 <template>
   <div class="login-container">
-    <!-- <ThemeSwitch class="theme-switch" /> -->
-    <!-- <Owl :close-eyes="isFocus" />  -->
     <div class="login-content">
       <div class="img-container">
         <img src="http://144.48.83.77:9002/xh_admin/images/登录界面/u14.png" />
@@ -113,41 +92,34 @@ createCode()
                 @focus="handleFocus"
               />
             </el-form-item>
-            <!-- <el-form-item prop="code">
-            <el-input
-              v-model.trim="loginFormData.code"
-              placeholder="验证码"
-              type="text"
-              tabindex="3"
-              :prefix-icon="Key"
-              maxlength="7"
-              size="large"
-            >
-              <template #append>
-                <el-image :src="codeUrl" @click="createCode" draggable="false">
-                  <template #placeholder>
-                    <el-icon>
-                      <Picture />
-                    </el-icon>
-                  </template>
-                  <template #error>
-                    <el-icon>
-                      <Loading />
-                    </el-icon>
-                  </template>
-                </el-image>
-              </template>
-            </el-input>
-          </el-form-item> -->
             <el-button :loading="loading" type="primary" size="large" @click.prevent="handleLogin">登 录</el-button>
           </el-form>
         </div>
       </div>
     </div>
+    <div class="footer">
+      <p>Copyright © www.AAAAAAA.com, All Rights Reserved.</p>
+      <p>助你快速打造友好美观的交互原型</p>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.img-container {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+@media (max-width: 820px) {
+  .img-container {
+    display: none;
+  }
+}
 .login-container {
   display: flex;
   flex-direction: column;
@@ -157,26 +129,20 @@ createCode()
   width: 100%;
   min-height: 100%;
 
+  .footer {
+    margin-top: 40px;
+
+    p {
+      color: #fff;
+      text-align: center;
+    }
+  }
   .login-content {
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 840px;
     height: 600px;
-    // box-shadow: 0 0 10px #dcdfe6;
-    // background-color: var(--el-bg-color);
     overflow: hidden;
-    .img-container {
-      width: 420px;
-      height: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
   }
   .theme-switch {
     position: fixed;
@@ -185,10 +151,9 @@ createCode()
     cursor: pointer;
   }
   .login-card {
+    padding: 0 25px;
     height: 100%;
-    width: 420px;
     background: #fff;
-    // background-color: var(--el-bg-color);
     overflow: hidden;
     .title {
       display: flex;
